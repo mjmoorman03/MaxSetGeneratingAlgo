119f0300667568f27745a2a8427995832f2d98c9
mjmoorman03 <90151067+mjmoorman03@users.noreply.github.com>
2022-10-24 20:35:14 -0400

    while

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,4 @@
+from hashlib import new
 import math
 import itertools 
 
@@ -7,6 +8,9 @@ def MaxSetGenerating(d, rotNum, orbit):
     p = rotNum[0]
     q = rotNum[1]
     orbit = orbit.sort()
+    newOrbit = orbit 
+    while newOrbit[0] != 0:
+        newOrbit = map(lambda x: x - 1, newOrbit)
 
 
     pass
